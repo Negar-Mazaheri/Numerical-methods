98b09e10f4ad2e7382fdb0ea94cde34c57200d23
Negar-Mazaheri <129887484+Negar-Mazaheri@users.noreply.github.com>
2024-02-19 23:32:27 +0330

    Update Lagrange Interpolation.py

--- Lagrange Interpolation.py
+++ Lagrange Interpolation.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[9]:
-
-
 def Lagrange(a, X, Y):
     """
     This function implements the Lagrange interpolation method.
@@ -30,37 +24,5 @@ def Lagrange(a, X, Y):
         yp += product * Y[i]
 
     return yp
-
-
-
-# In[11]:
-
-
-# Get inputs from the user
-X_input = input("Enter X values separated by spaces: ")
-Y_input = input("Enter Y values separated by spaces: ")
-a_input = input("Enter the value of 'a': ")
-
-# Convert input strings to lists
-X = [float(x) for x in X_input.split()]
-Y = [float(y) for y in Y_input.split()]
-a = float(a_input)
-
-# Call the function with user inputs
-result = Lagrange(a, X, Y)
-
-# Display the result
-print("Result:", result)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
 
 
